09807c43762ebd8a2c502294b71927b83f56c857
Psina0802 <apryavrpvar@gmail.com>
2024-10-03 21:29:28 +0500

    login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,14 +1,120 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, Input, OnInit, output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import {
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+  IonButtons,
+  IonButton,
+  IonModal,
+  IonIcon,
+  IonList,
+  IonItem,
+  IonLabel,
+  IonInput,
+  IonChip,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonAccordion,
+  IonSegment,
+  IonSegmentButton,
+  IonAccordionGroup,
+  IonFab,
+  IonFabButton,
+  IonFabList,
+  IonSearchbar,
+  IonProgressBar,
+  IonPopover,
+  IonSkeletonText,
+  IonCard,
+  IonToast,
+} from '@ionic/angular/standalone';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
+  standalone: true,
+  imports: [
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    IonButtons,
+    IonButton,
+    IonModal,
+    IonIcon,
+    IonList,
+    IonItem,
+    IonLabel,
+    IonInput,
+    IonChip,
+    IonGrid,
+    IonRow,
+    IonCol,
+    IonAccordion,
+    IonSegment,
+    IonSegmentButton,
+    IonAccordionGroup,
+    IonFab,
+    IonFabButton,
+    IonFabList,
+    IonSearchbar,
+    IonProgressBar,
+    IonPopover,
+    IonSkeletonText,
+    IonCard,
+    IonToast,
+    CommonModule,
+    FormsModule,
+  ],
 })
-export class LoginComponent  implements OnInit {
+export class LoginComponent implements OnInit {
+  open_toast: boolean = false;
+  message: string = '';
 
-  constructor() { }
+  isloading = false;
+
+  @Input() login_open = false;
+  @Input() signup_open = false;
+  @Input() user = {
+    username: '',
+    intiated_name: '',
+    password: '',
+    _password: '',
+    email: '',
+    phone: '',
+    lang_type: '',
+  };
+
+  loginOutput = output<boolean>();
+  constructor() {}
+
+  closeModal(x: boolean) {
+    this.loginOutput.emit(x);
+    console.log('компонент метод');
+  }
+  login() {}
+  signUp() {
+    if (this.user.password !== this.user._password) {
+      this.open_toast = true;
+      this.message = 'Passwords do not match';
+      return;
+    }
+    const atSymbolIndex = this.user.email.indexOf('@');
+    const dotIndex = this.user.email.lastIndexOf('.');
+    let check =
+      atSymbolIndex > 0 &&
+      dotIndex > atSymbolIndex + 1 &&
+      dotIndex < this.user.email.length - 1;
+    if (!check) {
+      this.open_toast = true;
+      this.message = 'Invalid Email';
+      return;
+    }
+  }
 
   ngOnInit() {}
-
 }
--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,53 +1,100 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonGrid, IonRow, IonCol, IonButton, IonChip, IonButtons } from '@ionic/angular/standalone';
+import {
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonButton,
+  IonChip,
+  IonButtons,
+  IonModal,
+} from '@ionic/angular/standalone';
 import { register } from 'swiper/element/bundle';
 import { YoutubePlayerComponent } from 'src/app/components/youtube-player/youtube-player.component';
 import { addIcons } from 'ionicons';
-import {logoInstagram, logoWhatsapp, logoFacebook, logoGoogle} from 'ionicons/icons'
+import { LoginComponent } from 'src/app/components/login/login.component';
+import {
+  logoInstagram,
+  logoWhatsapp,
+  logoFacebook,
+  logoGoogle,
+} from 'ionicons/icons';
 
-// register Swiper custom elements  
+// register Swiper custom elements
 register();
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
   standalone: true,
-  imports: [IonButtons, IonChip, IonButton, IonCol, IonRow, IonGrid, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, YoutubePlayerComponent],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+  imports: [
+    IonButtons,
+    IonChip,
+    IonButton,
+    IonCol,
+    IonRow,
+    IonGrid,
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    CommonModule,
+    FormsModule,
+    YoutubePlayerComponent,
+    LoginComponent,
+    IonModal,
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class HomePage implements OnInit {
- 
   messages: any[] = [
     {
       title: 'New batches start every month for Module 1 and Module 2.',
-      message: 'The most effective kirtan school will start new batches very soon. Tell your friend who would love to learn how to lead kirtan with Harmonium.',
-      link: "https://vasisstudio.com/kirtan-school",
-      icon: ""
-  
+      message:
+        'The most effective kirtan school will start new batches very soon. Tell your friend who would love to learn how to lead kirtan with Harmonium.',
+      link: 'https://vasisstudio.com/kirtan-school',
+      icon: '',
     },
-    
+
     {
       title: 'LOOK DOWN FOR THE BEST HOMEWORK OF THE WEEK!!!',
-      message: 'This week the best Homework for the week for the first Module is from our Student Kamalaksha, playing Jaya Radha Madhava',
-      link: "https://vasisstudio.com/kirtan-school",
-      icon: ""
+      message:
+        'This week the best Homework for the week for the first Module is from our Student Kamalaksha, playing Jaya Radha Madhava',
+      link: 'https://vasisstudio.com/kirtan-school',
+      icon: '',
     },
     {
       title: null,
       message: 'Have your rated us on Google? Give your Feedback !',
-      link: "https://vasisstudio.com/kirtan-school",
-      icon: ""
-    }
+      link: 'https://vasisstudio.com/kirtan-school',
+      icon: '',
+    },
   ];
+  user = {
+    username: 'Ramana',
+    intiated_name: 'Ramana',
+    password: 'Point123',
+    _password: 'Point123',
+    email: 'ramana@gmail.com',
+    phone: '+91 9876543210',
+    lang_type: 'English',
+  };
+  login_open: boolean = false;
+  signup_open: boolean = false;
+  profile_open: boolean = false;
 
-
-  constructor() { 
-    addIcons({logoInstagram,logoFacebook,logoGoogle,logoWhatsapp});
+  constructor() {
+    addIcons({ logoInstagram, logoFacebook, logoGoogle, logoWhatsapp });
   }
 
-  ngOnInit() {
+  open(x: any) {
+    this.login_open = false;
   }
 
+  ngOnInit() {}
 }
