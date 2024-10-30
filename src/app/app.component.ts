be728a2b9c50edc54da6dec33fb1afad4cbc6c98
Psina0802 <apryavrpvar@gmail.com>
2024-10-30 14:17:35 +0500

    edit for home page and tutorial page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,53 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
-import { IonApp, IonSplitPane, IonMenu, IonContent, IonList, IonListHeader, IonNote, IonMenuToggle, IonItem, IonIcon, IonLabel, IonRouterOutlet, IonRouterLink } from '@ionic/angular/standalone';
-
+import {
+  IonApp,
+  IonSplitPane,
+  IonMenu,
+  IonContent,
+  IonList,
+  IonListHeader,
+  IonNote,
+  IonMenuToggle,
+  IonItem,
+  IonIcon,
+  IonLabel,
+  IonRouterOutlet,
+  IonRouterLink,
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
   standalone: true,
-  imports: [RouterLink, RouterLinkActive, CommonModule, IonApp, IonSplitPane, IonMenu, IonContent, IonList, IonListHeader, IonNote, IonMenuToggle, IonItem, IonIcon, IonLabel, IonRouterLink, IonRouterOutlet],
+  imports: [
+    RouterLink,
+    RouterLinkActive,
+    CommonModule,
+    IonApp,
+    IonSplitPane,
+    IonMenu,
+    IonContent,
+    IonList,
+    IonListHeader,
+    IonNote,
+    IonMenuToggle,
+    IonItem,
+    IonIcon,
+    IonLabel,
+    IonRouterLink,
+    IonRouterOutlet,
+  ],
 })
 export class AppComponent {
-
   appPages = [
     { title: 'Home', url: '/home', icon: 'home' },
     { title: 'Management', url: '/management-dashboard', icon: 'home' },
     { title: 'Courses', url: '/course-dashboard', icon: 'home' },
-    { title: 'Tutorial', url: '/about', icon: 'information-circle' },
+    { title: 'Tutorial', url: '/tutorial', icon: 'information-circle' },
     { title: 'Contact', url: '/contact', icon: 'mail' },
   ];
-  constructor() {
-   
-  }
+  constructor() {}
 }
--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,6 +1,10 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
+
+import { HomeService } from 'src/app/services/home/home.service';
 import { FormsModule } from '@angular/forms';
+
+import { CommonModule } from '@angular/common';
+import { SwiperComponent } from 'src/app/components/swiper/swiper.component';
 import {
   IonContent,
   IonHeader,
@@ -13,6 +17,19 @@ import {
   IonChip,
   IonButtons,
   IonModal,
+  IonItem,
+  IonIcon,
+  IonCard,
+  IonCardHeader,
+  IonCardContent,
+  IonLabel,
+  IonMenuButton,
+  IonSelect,
+  IonSelectOption,
+  IonSegmentButton,
+  IonSegment,
+  IonList,
+  IonInput,
 } from '@ionic/angular/standalone';
 import { register } from 'swiper/element/bundle';
 import { YoutubePlayerComponent } from 'src/app/components/youtube-player/youtube-player.component';
@@ -24,6 +41,7 @@ import {
   logoFacebook,
   logoGoogle,
 } from 'ionicons/icons';
+import Swiper from 'swiper';
 
 // register Swiper custom elements
 register();
@@ -33,6 +51,21 @@ register();
   styleUrls: ['./home.page.scss'],
   standalone: true,
   imports: [
+    IonInput,
+    IonList,
+    IonSegmentButton,
+    IonSegment,
+    IonMenuButton,
+    IonSelect,
+    IonSelectOption,
+    IonLabel,
+    IonCardContent,
+    IonCardHeader,
+    IonCard,
+    IonIcon,
+    IonItem,
+    CommonModule,
+    FormsModule,
     IonButtons,
     IonChip,
     IonButton,
@@ -43,15 +76,21 @@ register();
     IonHeader,
     IonTitle,
     IonToolbar,
-    CommonModule,
-    FormsModule,
     YoutubePlayerComponent,
     LoginComponent,
     IonModal,
+    SwiperComponent,
   ],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class HomePage implements OnInit {
+  api = inject(HomeService);
+  img: string = 'banner0802.jpg';
+  text: any;
+  textForEdit: any;
+  displayedMessages: any;
+  video_id: string = 'https://www.youtube.com/watch?v=QCO9VSj4h18';
+  link: string = 'https://vasisstudio.com/kirtan-school';
+  selectLang = 'ENG';
   messages: any[] = [
     {
       title: 'New batches start every month for Module 1 and Module 2.',
@@ -77,13 +116,138 @@ export class HomePage implements OnInit {
   ];
 
   login_open: boolean = false;
-
+  edit_open: boolean = false;
+  edit_img_open: boolean = false;
+  selectedFile: File | null = null;
   constructor() {
     addIcons({ logoInstagram, logoFacebook, logoGoogle, logoWhatsapp });
+    this.getText();
   }
 
   open(x: any) {
     this.login_open = false;
+  }
+
+  async getText() {
+    let x = await this.api.getHome();
+    this.text = x.data;
+    this.video_id = 'https://www.youtube.com/watch?v=' + this.text[0].video_id;
+    this.img = this.text[3].img;
+    this.link = this.text[0].link;
+  }
+  _lang() {
+    console.log(this.selectLang);
+  }
+
+  edit_text() {
+    let array: any = [];
+    switch (this.selectLang) {
+      case 'ENG':
+        array = this.text?.map(
+          (element: {
+            id: number;
+            message_e: any;
+            video_title_e: any;
+            video_id: any;
+            link: any;
+          }) => {
+            return {
+              id: element.id,
+              message: element.message_e,
+              video_title: element.video_title_e,
+              video_id: element.video_id,
+              link: element.link,
+            };
+          }
+        );
+        break;
+      case 'SPN':
+        array = this.text?.map(
+          (element: {
+            id: number;
+            message_s: any;
+            video_title_s: any;
+            video_id: any;
+            link: any;
+          }) => {
+            return {
+              id: element.id,
+              message: element.message_s,
+              video_title: element.video_title_s,
+              video_id: element.video_id,
+              link: element.link,
+            };
+          }
+        );
+        break;
+      case 'POR':
+        array = this.text?.map(
+          (element: {
+            id: number;
+            message_p: any;
+            video_title_p: any;
+            video_id: any;
+            link: any;
+          }) => {
+            return {
+              id: element.id,
+              message: element.message_p,
+              video_title: element.video_title_p,
+              video_id: element.video_id,
+              link: element.link,
+            };
+          }
+        );
+    }
+    this.textForEdit = array;
+    console.log(this.textForEdit);
+  }
+
+  saveEdit(
+    page_text: {
+      id: number;
+      message: string;
+      video_title: string;
+      video_id: string;
+      link: string;
+    }[]
+  ) {
+    console.log(page_text);
+    switch (this.selectLang) {
+      case 'ENG':
+        this.api.editTextEng(page_text);
+        break;
+      case 'SPN':
+        this.api.editTextSp(page_text);
+        break;
+      case 'POR':
+        this.api.editTextPor(page_text);
+        break;
+    }
+  }
+
+  // async getImage() {
+  //   let x = await this.api.getImage(this.img);
+
+  //   if (x !== null) {
+  //     this.img = x;
+  //   }
+  //   console.log('image', this.img);
+  // }
+
+  onFileSelected(event: any) {
+    const file: File = event.target.files[0];
+    if (file) {
+      this.selectedFile = file;
+      console.log('Файл выбран:', file);
+    }
+  }
+
+  async uploadImage() {
+    let x = await this.api.uploadImage(this.selectedFile, 4);
+    if (x !== null) {
+      this.img = x;
+    }
   }
 
   ngOnInit() {}
