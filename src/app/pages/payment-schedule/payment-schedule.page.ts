e80b7b3f16af8aecb6ae0b3574e59775ebddcb4a
Psina0802 <apryavrpvar@gmail.com>
2025-04-18 14:22:43 +0500

    payment lang and scss

--- src/app/pages/payment-schedule/payment-schedule.page.ts
+++ src/app/pages/payment-schedule/payment-schedule.page.ts
@@ -2,45 +2,56 @@ import { Component, inject, OnInit, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonCol, IonRow, IonGrid } from '@ionic/angular/standalone';
-import { HeaderComponent } from "../../components/header/header.component";
+import { HeaderComponent } from '../../components/header/header.component';
 import { CrudService } from '../../services/crud';
 import { LoginService } from '../../services/auth/login.service';
-import { TeacherViewComponent } from "./components/teacher-view/teacher-view.component";
-import { PaymentSessionsComponent } from "./components/payment-sessions/payment-sessions.component";
+import { TeacherViewComponent } from './components/teacher-view/teacher-view.component';
+import { PaymentSessionsComponent } from './components/payment-sessions/payment-sessions.component';
+import { TranslatePipe, TranslateService } from '@ngx-translate/core';
 @Component({
   selector: 'app-payment-schedule',
   templateUrl: './payment-schedule.page.html',
   styleUrls: ['./payment-schedule.page.scss'],
   standalone: true,
-  imports: [IonGrid, IonRow, IonCol, IonContent, CommonModule, FormsModule, HeaderComponent, TeacherViewComponent, PaymentSessionsComponent]
+  imports: [
+    IonGrid,
+    IonRow,
+    IonCol,
+    IonContent,
+    CommonModule,
+    FormsModule,
+    HeaderComponent,
+    TeacherViewComponent,
+    PaymentSessionsComponent,
+    TranslatePipe,
+  ],
 })
 export class PaymentSchedulePage implements OnInit {
+  translate = inject(TranslateService);
   api = inject(CrudService);
   auth = inject(LoginService);
   groups: any[] = [];
   group_name: string = '';
   selectedGroup = signal<any>(null);
-  constructor() { 
+  constructor() {
     if (this.auth._user?.is_admin) {
       this.getPaymentScheduleAdmin();
     }
   }
 
-  ngOnInit() {
-  }
-
-
+  ngOnInit() {}
 
   async getPaymentScheduleAdmin() {
     const response = await this.api.getMany('student_groups', {
-      filters: [{
-        column: 'status',
-        value: true
-      }]
+      filters: [
+        {
+          column: 'status',
+          value: true,
+        },
+      ],
     });
     if (response.data) {
       this.groups = response.data;
     }
   }
-
 }
